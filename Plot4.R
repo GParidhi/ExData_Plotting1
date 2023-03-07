cc6bf3a12bc70424c073eb0328d1ebc3da0bf0a4
GParidhi <98548991+GParidhi@users.noreply.github.com>
2023-03-07 15:34:58 +0530

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -13,3 +13,15 @@ Sub_metering_3 <- as.numeric(subsetofdata$Sub_metering_3)
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2)) 
 
+with(subpower
+{
+  plot(subsetofdata$Time,as.numeric(as.character(subsetofdata$Global_active_power)),type="l",  xlab="",ylab="Global Active Power")  
+  plot(subsetofdata$Time,as.numeric(as.character(subsetofdata$Voltage)), type="l",xlab="datetime",ylab="Voltage")
+  plot(subsetofdata$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+   with(subsetofdata,lines(Time,as.numeric(as.character(Sub_metering_1))))
+   with(subsetofdata,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+   with(subsetofdata,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+   legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex = 0.6)
+  plot(subsetofdata$Time,as.numeric(as.character(subsetofdata$Global_reactive_power)),type="l",xlab="datetime",ylab="Global_reactive_power")
+})
+dev.off()
